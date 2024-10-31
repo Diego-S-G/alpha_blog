272a5f1124093515fcd222686c2f92f8d4440c2c
Diego-S-G <diego07goncalves@gmail.com>
2024-10-31 15:24:36 -0300

    Adiciona associação Um-pra-Muitos entre Users e Articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first # por enquanto
     if @article.save
       flash[:notice] = "Artigo criado com sucesso!"
       redirect_to @article
