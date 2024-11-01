64610a6d1deeca0587b577e97a00edac5aee80de
Diego-S-G <diego07goncalves@gmail.com>
2024-11-01 14:05:02 -0300

    Adiciona show page de Usuários, usando o helper gravatar_for criado para mostrar a foto de perfil associada ao email e faz partial da lista de artigos pra usar no index de artigos e na user page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def gravatar_for(user)
+    email_address = user.email.downcase
+    hash = Digest::SHA256.hexdigest(email_address)
+    size = 200
+
+    params = URI.encode_www_form("s" => size)
+
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?#{params}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end
 end
