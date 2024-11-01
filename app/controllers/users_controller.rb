f62c10da0fad9956db287edf87819247fe934239
Diego-S-G <diego07goncalves@gmail.com>
2024-11-01 11:13:10 -0300

    Implementa a ação create para usuários e adiciona validação de tamanho para senha.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Bem-vindo(a) ao Alpha Blog, #{@user.username}!"
+      redirect_to root_path
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 
   has_secure_password
+
+  validates :password, length: { minimum: 6, maximum: 20 }
 end
