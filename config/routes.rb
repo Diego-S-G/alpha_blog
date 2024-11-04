a45864b87d6789ddc6b5023747bca114df47b471
Diego-S-G <diego07goncalves@gmail.com>
2024-11-04 10:02:21 -0300

    Cria rotas new, create e destroy e controller para sessions, além de fazer a página de Login de usuário.

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,8 @@ Rails.application.routes.draw do
   resources :users, except: [ :new ]
 
   get "signup" => "users#new"
+
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
 end
