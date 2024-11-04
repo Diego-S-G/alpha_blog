af88455130b316047b6f4255b91c0f1caee809c0
Diego-S-G <diego07goncalves@gmail.com>
2024-11-04 11:09:50 -0300

    Faz método create e destroy com base em sessões de usuários. Adiciona login e logout na topbar.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,22 @@
 class SessionsController < ApplicationController
   def new
-    
   end
 
   def create
-    
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Usuário logado com sucesso!"
+      redirect_to user
+    else
+      flash.now[:alert] = "Algo de errado aconteceu com seu Login"
+      render "new", status: :unprocessable_entity
+    end
   end
 
   def destroy
-    
+    session[:user_id] = nil
+    flash[:notice] = "Usuário desconectado."
+    redirect_to root_path
   end
 end
