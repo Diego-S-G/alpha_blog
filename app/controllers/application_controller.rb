815188b4ec304666726984e4b185789e25f4e447
Diego-S-G <diego07goncalves@gmail.com>
2024-11-04 14:38:23 -0300

    Adiciona restrições nos artigos: pessoas não logadas não conseguem burlar criação e edição e pessoas logadas não conseguem burlar editar, apagar artigos que não são seus.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :require_user
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -10,5 +10,12 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "Você precisa estar logado para fazer esta ação!"
+      redirect_to login_path
+    end
   end
 end
